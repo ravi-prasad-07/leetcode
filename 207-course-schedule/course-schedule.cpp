6a9fe56126c3dd083e3ff5632b8efa84f0765020
Ravi Kumar <rkumar497421@gmail.com>
2026-03-23 22:44:27 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (93.91%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -25,12 +25,12 @@ bool isCyclic(vector<int> adj[],vector<int>& visit,vector<int>& pathvisit,int ve
     for(int i=0;i<ver;i++){
         if(!visit[i]){
             if(dfs(adj,visit,pathvisit,i)){
-                return true;
+                return false;
             }
         }
     }
 
-    return false;
+    return true;
 }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -43,38 +43,6 @@ bool isCyclic(vector<int> adj[],vector<int>& visit,vector<int>& pathvisit,int ve
         vector<int> visited(numCourses, 0);
         vector<int> pathvisit(numCourses, 0);
 
-        // for(int i=0;i<numCourses;i++){
-        //     for(auto it:adj[i]){
-        //         visited[it]++;
-        //     }
-        // }
-
-        // queue<int> q;
-        // for(int i=0;i<numCourses;i++){
-        //     if(visited[i]==0){
-        //         q.push(i);
-        //     }
-        // }
-
-        // vector<int> topo;
-        // while(!q.empty()){
-        //     int node=q.front();
-        //     q.pop();
-        //     topo.push_back(node);
-
-        //     for(auto it: adj[node]){
-        //         visited[it]--;
-        //         if(visited[it]==0){
-        //             q.push(it);
-        //         }
-        //     }
-        // }
-
-        // if(topo.size()==numCourses){
-        //     return true;
-        // }
-
-        // return false;
-        return !isCyclic(adj,visited,pathvisit,numCourses);;
+        return isCyclic(adj,visited,pathvisit,numCourses);
     }
 };
