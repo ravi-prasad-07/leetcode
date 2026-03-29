af0b13a1a3f29a835d0d7574cebb3c388dd9bef3
Ravi Kumar <rkumar497421@gmail.com>
2026-03-29 20:19:37 +0530

    Time: 2 ms (29.88%) | Memory: 31.4 MB (45.59%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     bool areSimilar(vector<vector<int>>& ori, int k) {
-        vector<vector<int>> mat=ori;
-        int m=mat.size();//row
-        int n=mat[0].size();//column
-        int temp;
+        int m=ori.size();
+        int n=ori[0].size();
 
-        while(k>0){
-            for(int i=0;i<m;i++){
-                if(i%2==0){
-                    temp=mat[i][0];
-                    for(int j=0;j<n-1;j++){
-                        mat[i][j]=mat[i][j+1];
-                    }
-                    mat[i][n-1]=temp;
-                }
-                else{
-                    temp=mat[i][n-1];
-                    for(int j=n-1;j>0;j--){
-                        mat[i][j]=mat[i][j-1];
-                    }
-                    mat[i][0]=temp;
-                }
-            }
-            k--;
-        }
+        k =k%n;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(ori[i][j]!=mat[i][j]){
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                int newCol;
+
+                if(i%2==0) {
+                    newCol=(j+k)%n;
+                } else {
+                    newCol=(j-k+n)%n;
+                }
+
+                if(ori[i][j]!=ori[i][newCol]) {
                     return false;
                 }
             }
