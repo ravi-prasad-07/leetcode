12a9e60323b6c9884517f278623b727eeeef51d2
Ravi Kumar <rkumar497421@gmail.com>
2026-06-21 15:05:22 +0530

    Time: 20 ms (74.51%) | Memory: 149.1 MB (61.13%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,21 +1,48 @@
 class NumMatrix {
-private:
-    int row, col;
-    vector<vector<int>> sums;
 public:
-    NumMatrix(vector<vector<int>> &matrix) {
-        row = matrix.size();
-        col = row>0 ? matrix[0].size() : 0;
-        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
-        for(int i=1; i<=row; i++) {
-            for(int j=1; j<=col; j++) {
-                sums[i][j] = matrix[i-1][j-1] + 
-                             sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
+
+    vector<vector<int>> ans;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        ans.resize(n+1, vector<int>(m+1));
+
+        ans[0][0]=matrix[0][0];
+        
+        for(int i=1;i<m;i++){
+            ans[0][i]=ans[0][i-1]+matrix[0][i];
+        }
+        for(int i=1;i<n;i++){
+            ans[i][0]=ans[i-1][0]+matrix[i][0];
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                ans[i][j]=matrix[i][j]+ans[i-1][j]-ans[i-1][j-1]+ans[i][j-1];
             }
         }
     }
-
+    
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+        int res = ans[row2][col2];
+
+        if(row1>0){
+            res-=ans[row1-1][col2];
+        }
+        if(col1>0){
+            res-=ans[row2][col1-1];
+        }
+        if (row1>0 && col1>0){
+            res+=ans[row1-1][col1-1];
+        }
+
+        return res;
     }
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
