ca9ca6ac7cc19fe03017452479b5f9de1603f911
Ravi Kumar <rkumar497421@gmail.com>
2026-09-14 22:41:07 +0530

    Update best-time-to-buy-and-sell-stock-ii.cpp

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int profit=0;
-
+        //pure greedy approach
+        //just add profits in profit variable as we hit the condition
         for(int i=1; i<prices.size(); i++){
             if(prices[i]>prices[i-1]){
                 profit+=prices[i]-prices[i-1];
