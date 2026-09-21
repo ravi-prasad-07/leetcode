cfeaea836ef24a3c6488b55825da99f77ba3cc0a
Ravi Kumar <rkumar497421@gmail.com>
2026-09-21 22:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (84.98%) - LeetSync

---
+++ 933-increasing-order-search-tree/increasing-order-search-tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* curr;
+
+    void inorder(TreeNode* root){
+        if(root==nullptr){
+            return;
+        }
+
+        inorder(root->left);
+
+        root->left=nullptr;
+        curr->right=root;
+        curr=curr->right;
+
+        inorder(root->right);
+    }
+
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* dummy=new TreeNode(0);
+        curr=dummy;
+        inorder(root);
+
+        return dummy->right;
+    }
+};
